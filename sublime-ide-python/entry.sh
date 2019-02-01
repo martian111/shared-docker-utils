36c1e01abcde775301db507d906dcc07d10c8539
Martin Lui <devel@martian111.net>
2019-01-31 23:54:26 -0800

    Added support for multi-package Python projects
    Previously, it's assumed that this will be used for single-package
    projects.  That is, a single /src/requirements.txt, /src/setup.py, etc.
    This adds support for multi-package projects that are mounted as /src.
    Such projects are detected by searching for
    /src/<package_name>/requirements{,-dev}.txt and creating the "venv"
    folder within that package's path.
    
    Minor enhancements includes:
    - Search for and use requirements-dev.txt if available.
    - Ability to use /bin/docker-entry-post-venv script to perform extra
      actions after the venv is set up (via pip)

--- sublime-ide-python/entry.sh
+++ sublime-ide-python/entry.sh
@@ -5,12 +5,34 @@ _port=${ANACONDA_PORT:-19360}
 
 
 cd /src
-if [[ ! -d /src/venv ]]; then
-    echo "Initializing venv..."
-    python -m venv venv
+if [[ -f /src/${PROJECT_NAME}/requirements.txt || \
+      -f /src/${PROJECT_NAME}/requirements-dev.txt ]]; then
+    _project_base=/src/${PROJECT_NAME}
+else
+    _project_base=/src
 fi
 
-/src/venv/bin/pip install --upgrade pip
-/src/venv/bin/pip install -r /src/requirements.txt
+_venv=${_project_base}/venv
+if [[ ! -d ${_venv} ]]; then
+    echo "Initializing venv..."
+    python -m venv ${_venv} || exit 1
+fi
 
-exec /src/venv/bin/python /opt/anaconda/anaconda_server/minserver.py -e /src/venv/ $_port
+_pip=${_venv}/bin/pip
+${_pip} install --upgrade pip  || exit 2
+if [[ -f ${_project_base}/requirements-dev.txt ]]; then
+    ${_pip} install -r ${_project_base}/requirements-dev.txt || exit 3
+else
+    ${_pip} install -r ${_project_base}/requirements.txt || exit 4
+fi
+
+
+# Run post-venv setup script, if any
+if [[ -x /bin/docker-entry-post-venv ]]; then
+    source /bin/docker-entry-post-venv || exit 10
+fi
+
+
+# Start Anaconda Server (for IDE)
+exec ${_venv}/bin/python /opt/anaconda/anaconda_server/minserver.py \
+    -e ${_venv}/ $_port
--- sublime-ide-python/python-in-docker.sh
+++ sublime-ide-python/python-in-docker.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-#project=$1; shift
 project=${PROJECT_NAME}
 
 new_args=()
 for var in "$@"
 do
     if [[ $var = *"/$project/"* ]]; then
-       #echo found $var
-       new_var="/src/${var#*/${project}/}"
-       #echo $var to $new_var
-       new_args+=("$new_var")
+        #echo found $var
+        if [[ -d "/src/${project}/venv" ]]; then
+            # Ref:
+            # https://www.thegeekstuff.com/2010/07/bash-string-manipulation/
+            new_var="/src/${project}/${var##*/${project}/}"
+        else
+            new_var="/src/${var#*/${project}/}"
+        fi
+        #echo $var to $new_var
+        new_args+=("$new_var")
     else
-       new_args+=("$var")
+        new_args+=("$var")
     fi
 done
 
-/src/venv/bin/python "${new_args[@]}"
+if [[ -d "/src/${project}/venv" ]]; then
+    /src/${project}/venv/bin/python "${new_args[@]}"
+else
+    /src/venv/bin/python "${new_args[@]}"
+fi
