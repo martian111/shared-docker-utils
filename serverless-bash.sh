39803bcc8b922abbdc53019edcfe4756ea6cc445
Martin Lui <devel@martian111.net>
2019-05-05 15:52:05 -0700

    Added serverless support for 'python3.6' runtime in AWS
    To use this, source the serverless-bash.sh script in .bash_profile,
    which will provide the 'sls' command/function in Bash. That function
    will run the correct Docker container based on the runtime configured in
    the serverless.yml file (sls command must be run from the root directory
    of the serverless project).

---
+++ serverless-bash.sh
@@ -0,0 +1,12 @@
+function sls() {
+    local _runtime=""
+    if [[ -f serverless.yml ]]; then
+        _runtime=`grep "runtime:" serverless.yml | cut -d ":" -f 2 | tr -d '[:space:]'`
+    fi
+
+    if [[ $_runtime = "python3.6" ]]; then
+        docker run --rm -v $HOME/.aws/config:/root/.aws/credentials -v $PWD:/src martian111/serverless-python3.6 "$@"
+    else
+        docker run --rm -v $HOME/.aws/config:/root/.aws/credentials -v $PWD:/src martian111/serverless "$@"
+    fi
+}
